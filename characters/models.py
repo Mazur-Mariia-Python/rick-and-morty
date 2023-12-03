37fa01ba2bdfab78ad7f5c0245477fd62978f0f6
Mazur Mariya <mazurinka29@gmail.com>
2023-12-03 20:15:10 +0200

    Added DRF & first view - random character implemented

--- characters/models.py
+++ characters/models.py
@@ -7,7 +7,6 @@ class Character(models.Model):
         DEAD = "Dead"
         UNKNOWN = "unknown"
 
-    # Female', 'Male', 'Genderless' or 'unknown'
     class GenderChoices(models.TextChoices):
         FEMALE = "Female"
         MALE = "Male"
@@ -16,17 +15,11 @@ class Character(models.Model):
 
     """api_id - the id of the character from "https://rickandmortyapi.com/api/character"""
     api_id = models.IntegerField(unique=True)
-    # name	string	The name of the character.
     name = models.CharField(max_length=255)
-    # status	string	The status of the character ('Alive', 'Dead' or 'unknown').
     status = models.CharField(max_length=50, choices=StatusChoices.choices)
-    # species	string	The species of the character.
     species = models.CharField(max_length=255)
-    # gender	string	The gender of the character ('Female', 'Male', 'Genderless' or 'unknown').
     gender = models.CharField(max_length=50, choices=GenderChoices.choices)
-    # image	string (url)	Link to the character's image. All images are 300x300px
-    # and most are medium shots or portraits since they are intended to be used as avatars.
-    image = models.URLField(max_length=255, unique=True)
+    image = models.URLField(max_length=255, unique=False)
 
     def __str__(self):
         return self.name

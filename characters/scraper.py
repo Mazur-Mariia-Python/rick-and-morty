c06454c7a5056ff9149b6f1ed9e337fd369c6300
Mazur Mariya <mazurinka29@gmail.com>
2023-12-04 09:33:49 +0200

    Added Character list endpoint with filter

--- characters/scraper.py
+++ characters/scraper.py
@@ -4,25 +4,13 @@ import requests
 
 from django.conf import settings
 
-from characters.models import Character
+from .models import Character
 
-# scrape_characters відповідає тільки за скрапінг інфо з певної api, щоб її дістати звідти
+
 def scrape_characters() -> List[Character]:
-    # don't use directly links
-    # characters_response = requests.get("https://rickandmortyapi.com/api/character")
-
     next_url_to_scrape = settings.RICK_AND_MORTY_API_CHARACTERS_URL
 
-    # Best practice. Краще зберігати посилання на сайт у settings.py
-    # characters_response = requests.get(settings.RICK_AND_MORTY_API_CHARACTERS_URL).json()
-
-
-    # return characters_response
-    # Ми не зможемо запустити цю програмку, бо вона залежить від Django. Але її
-    # можна запустити через Python Console. Це одночасно і Django Console, тому
-    # є доступ до django.conf import settings і до модельок.
     characters = []
-    # Логіка в тому, щоб ітеруватися доти поки next буде рівним null. Тоді зберемо інфо про всіх персонажів charactersз усіх сторінок
     while next_url_to_scrape is not None:
         characters_response = requests.get(next_url_to_scrape).json()
 
@@ -41,14 +29,11 @@ def scrape_characters() -> List[Character]:
     return characters
 
 
-# функція для зберігання персонажів
 def save_characters(characters: List[Character]) -> None:
     for character in characters:
         character.save()
 
 
-# функція, яка спочатку скрапить персонажів з апіхи, а потів вже зберігає їх у БД
 def sync_characters_with_api() -> None:
     characters = scrape_characters()
     save_characters(characters)
-
--- characters/views.py
+++ characters/views.py
@@ -1,12 +1,12 @@
 import random
 
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from characters.models import Character
-from characters.serializers import CharacterSerializer
+from .models import Character
+from .serializers import CharacterSerializer
 
 
 @api_view(["GET"])
@@ -16,3 +16,32 @@ def get_random_character_view(request: Request) -> Response:
     random_character = Character.objects.get(pk=random_pk)
     serializer = CharacterSerializer(random_character)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# class PurchaseList(generics.ListAPIView):
+#     serializer_class = PurchaseSerializer
+#
+#     def get_queryset(self):
+#         """
+#         Optionally restricts the returned purchases to a given user,
+#         by filtering against a `username` query parameter in the URL.
+#         """
+#         queryset = Purchase.objects.all()
+#         username = self.request.query_params.get('username')
+#         if username is not None:
+#             queryset = queryset.filter(purchaser__username=username)
+#         return queryset
+
+
+class CharacterListView(generics.ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        queryset = Character.objects.all()
+
+        name = self.request.query_params.get("name")
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+            # queryset = queryset.filter(name__contains=name)
+            # queryset = queryset.filter(name=name)
+        return queryset
